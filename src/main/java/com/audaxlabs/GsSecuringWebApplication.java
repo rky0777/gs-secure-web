b9ea18e047701bb15a76b0564c0b6e5eeab4b72c
rky0777 <ryrohit93@gmail.com>
2024-02-18 15:13:50 +0530

    First Commit

---
+++ src/main/java/com/audaxlabs/GsSecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.audaxlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSecuringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsSecuringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audaxlabs/GsSecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audaxlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
